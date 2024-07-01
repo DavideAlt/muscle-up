e6192ce45b2e3972e11536f85d351fab8b099f96
Davide Carita <davide.carita@alten.it>
2024-07-01 16:01:36 +0200

    blank components

---
+++ src/app/components/exercises/exercises.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mu-exercises',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercises.component.html',
+  styleUrl: './exercises.component.scss'
+})
+export class ExercisesComponent {
+
+}
---
+++ src/app/components/tools/tools.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mu-tools',
+  standalone: true,
+  imports: [],
+  templateUrl: './tools.component.html',
+  styleUrl: './tools.component.scss'
+})
+export class ToolsComponent {
+
+}
