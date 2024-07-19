8fc006725efdbcd32783bef1a46f177ad48b46b0
Davide Carita <davide.carita@alten.it>
2024-07-19 12:55:02 +0200

    routing and 404 component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ExercisesComponent } from './components/exercises/exercises.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'exercises', component: ExercisesComponent},
+    {path: 'exercises/:muscle', component: ExercisesComponent},
+    {path: 'exercises/:muscle/:equipment', component: ExercisesComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent},
+];
