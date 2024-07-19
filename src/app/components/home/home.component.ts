21daaf9bc3b448ed373d13bcc51845c52675a9d1
Davide Carita <davide.carita@alten.it>
2024-07-19 10:25:43 +0200

    bodymap test in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { ResponsiveService } from '../../services/responsive.service';
+import { BodymapComponent } from "../bodymap/bodymap.component";
 
 @Component({
   selector: 'mu-home',
   standalone: true,
-  imports: [],
+  imports: [
+    BodymapComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
