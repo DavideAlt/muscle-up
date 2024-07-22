1580c8d2cd359e0d494bfb239ee68a30788a29f6
Davide Carita <davide.carita@alten.it>
2024-07-22 11:59:57 +0200

    added routes and about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ExercisesComponent } from './components/exercises/exercises.component';
 import { HomeComponent } from './components/home/home.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'exercises', component: ExercisesComponent},
     {path: 'exercises/:muscle', component: ExercisesComponent},
     {path: 'exercises/:muscle/:equipment', component: ExercisesComponent},
+    {path: 'tools', component: ToolsComponent},
+    {path: 'about', component: AboutComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: '**', component: PageNotFoundComponent},
 ];
