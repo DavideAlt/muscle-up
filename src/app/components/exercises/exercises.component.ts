11c04ef80075a608e1ab85f00987c2cb7286a6b3
Davide Carita <davide.carita@alten.it>
2024-07-23 12:01:16 +0200

    input muscleGroup from url

--- src/app/components/exercises/exercises.component.ts
+++ src/app/components/exercises/exercises.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'mu-exercises',
@@ -7,25 +8,42 @@ import { Component } from '@angular/core';
   templateUrl: './exercises.component.html',
   styleUrl: './exercises.component.scss'
 })
-export class ExercisesComponent {
+export class ExercisesComponent implements OnInit{
 
-  public exs = [
-                'abdominals',
-                'biceps',
-                'calves',
-                'chest',
-                'forearms',
-                'front-shoulders',
-                'glutes',
-                'hamstrings',
-                'hands',
-                'lats',
-                'lowerback',
-                'obliques',
-                'quads',
-                'rear-shoulders',
-                'traps',
-                'traps-middle',
-                'triceps'
-              ];
+  @Input() muscleGroup: string = 'all';
+
+  public muscleGroups = [
+    'abdominals',
+    'biceps',
+    'calves',
+    'chest',
+    'forearms',
+    'front-shoulders',
+    'glutes',
+    'hamstrings',
+    'hands',
+    'lats',
+    'lowerback',
+    'obliques',
+    'quads',
+    'rear-shoulders',
+    'traps',
+    'traps-middle',
+    'triceps'
+  ];
+
+  constructor(
+    private _route: ActivatedRoute
+  ) {
+    //
+  }
+
+  ngOnInit(): void {
+    this._route.url.subscribe(segments => {
+      let muscleGroup = segments[segments.length-1].toString();
+      if (this.muscleGroups.includes(muscleGroup)) {
+        this.muscleGroup = muscleGroup;
+      }
+    });
+  }
 }
