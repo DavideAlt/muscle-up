0306fa3ff218c0298596a1cb7a73ecccdefa09f4
Davide Carita <davide.carita@alten.it>
2024-07-29 15:37:15 +0200

    scroll to top on navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterContentChecked, AfterViewInit, Component } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,21 @@ import { FooterComponent } from './components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked {
+
+  constructor(
+    private _router: Router,
+    private _viewportScroller: ViewportScroller
+  ) {
+    //
+  }
+  ngAfterContentChecked(): void {
+    this._router.events.subscribe(ev => {
+      if (ev instanceof NavigationEnd) {
+        this._viewportScroller.scrollToPosition([0, 0]);
+        window.dispatchEvent(new Event('resize'));
+      }
+    })
+  }
   
 }
